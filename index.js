9b307227ad5f4cf7196d837ef74854d46123e30a
rukhsarnaveed12 <mk8076750@gmail.com>
2024-09-30 04:43:02 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//ALERT//
+alert("This function will display test in a dialog box that pops up on the screen");
+
+alert("Welcome to my website");
+
+alert("Capitalize doesn't matter");
+
+alert("Hello,World!");
+
+alert("Really need to alert some underline");
+
+
